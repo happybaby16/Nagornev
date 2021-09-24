142c6f4e2f02a67be9b3ccb6899cb88693315924
НагорневПМ@ngknn.local <@ngknn.local>
2021-09-24 16:21:32 +0300

    Добавьте файлы проекта.

---
+++ UserData/pages/pageRegLog/pageLogin.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserData.pages.pageRegLog
+{
+    /// <summary>
+    /// Логика взаимодействия для pageLogin.xaml
+    /// </summary>
+    public partial class pageLogin : Page
+    {
+        public pageLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReg_Click(object sender, RoutedEventArgs e)
+        {
+            User.mainFrame.Navigate(new pageRegistration());
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            User.mainFrame.Navigate(new pageInfoUser());
+        }
+    }
+}
---
+++ UserData/pages/pageRegLog/pageRegistration.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserData.pages.pageRegLog
+{
+    /// <summary>
+    /// Логика взаимодействия для pageRegistration.xaml
+    /// </summary>
+    public partial class pageRegistration : Page
+    {
+        public pageRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSendUser(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
