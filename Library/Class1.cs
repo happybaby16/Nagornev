00a818a7e0fb42e3d2ee6390db044b17846a4e6b
Pavel Nagornev <pavel.nagornev@mail.ru>
2021-10-23 17:51:47 +0300

    AgeAVG

---
+++ Library/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public static class Func
+    {
+        public static double AgeAVG(List<DateTime> usersDateBirth)
+        {
+            double avg = 0;
+            int counter = 0;
+            DateTime now = DateTime.Now;
+            foreach (DateTime BirthDay in usersDateBirth)
+            {
+                try
+                {
+                    int temp = (now - BirthDay).Days;
+                    avg += (temp / 365);
+                    counter++;
+                }
+                catch{}
+            }
+            return avg/counter;
+        }
+    }
+}
