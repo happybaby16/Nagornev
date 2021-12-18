c2771405b39914c7dbb9f14f738658442125ebc0
Pavel Nagornev <pavel.nagornev@mail.ru>
2021-12-18 18:03:14 +0300

    Капча при авторизации

--- UserData/pages/pageRegLog/pageLogin.xaml.cs
+++ UserData/pages/pageRegLog/pageLogin.xaml.cs
@@ -22,11 +22,27 @@ namespace UserData.pages.pageRegLog
     /// </summary>
     public partial class pageLogin : Page
     {
+        private void CreateCaptcha()
+        {
+            willAnswer = 0;
+            int temp = r.Next(0, 9);
+            txtCaptcha.Text = temp.ToString();
+            willAnswer += temp;
+            txtCaptcha.Text += " + ";
+            temp = r.Next(0, 9);
+            txtCaptcha.Text += temp.ToString();
+            willAnswer += temp;
+        }
+        Random r = new Random();
+        int willAnswer;//Captcha answer
+
+
         public pageLogin()
         {
             InitializeComponent();
             txtLogin.Text = "pupsik";
             txtPassword.Password = "123";
+            CreateCaptcha();
         }
 
         private void btnReg_Click(object sender, RoutedEventArgs e)
@@ -36,33 +52,54 @@ namespace UserData.pages.pageRegLog
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
-            SHA256 mySHA256 = SHA256.Create();
-            byte[] passwordByte = mySHA256.ComputeHash(Encoding.UTF8.GetBytes(txtPassword.Password));
-            string passwordHash = Convert.ToBase64String(passwordByte);
-            auth currentUser = DB.DataBase.auth.FirstOrDefault(x => x.login == txtLogin.Text && x.password == passwordHash);
-            if (currentUser != null)
+            try
             {
-                MessageBox.Show($"Добро пожаловать, {currentUser.login}!");
-                DB.currentUser = currentUser;
-                if (DB.currentUser.role == 1)
+                if (willAnswer.GetHashCode() == Convert.ToInt32(txtAnswerCaptcha.Text).GetHashCode())
                 {
-                    User.mainFrame.Navigate(new pageAdminMenuV2());
-                    User.mainWindow.imgAddBanner.Visibility = Visibility.Hidden;
-                    User.mainWindow.lbBanner.Visibility = Visibility.Hidden;
-                    User.mainWindow.mainGrid.ColumnDefinitions.RemoveAt(0);
-                    User.mainWindow.mainGrid.RowDefinitions.RemoveAt(2);
-                }
-                else if (DB.currentUser.role == 2)
-                {
-                    User.mainFrame.Navigate(new pageInfoUser());
-                }
+                    SHA256 mySHA256 = SHA256.Create();
+                    byte[] passwordByte = mySHA256.ComputeHash(Encoding.UTF8.GetBytes(txtPassword.Password));
+                    string passwordHash = Convert.ToBase64String(passwordByte);
+                    auth currentUser = DB.DataBase.auth.FirstOrDefault(x => x.login == txtLogin.Text && x.password == passwordHash);
+                    if (currentUser != null)
+                    {
+                        MessageBox.Show($"Добро пожаловать, {currentUser.login}!");
+                        DB.currentUser = currentUser;
+                        if (DB.currentUser.role == 1)
+                        {
+                            User.mainFrame.Navigate(new pageAdminMenuV2());
+                            User.mainWindow.imgAddBanner.Visibility = Visibility.Hidden;
+                            User.mainWindow.lbBanner.Visibility = Visibility.Hidden;
+                            User.mainWindow.mainGrid.ColumnDefinitions.RemoveAt(0);
+                            User.mainWindow.mainGrid.RowDefinitions.RemoveAt(2);
+                        }
+                        else if (DB.currentUser.role == 2)
+                        {
+                            User.mainFrame.Navigate(new pageInfoUser());
+                        }
 
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неверно введен логин или пароль!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Неверно введена капча, повторите попытку!");
+                    CreateCaptcha();
+                }
             }
-            else
+            catch (FormatException)
             {
-                MessageBox.Show("Неверно введен логин или пароль!");
+                MessageBox.Show("Неверно введена капча, повторите попытку!");
+                CreateCaptcha();
             }
             
+        }
+
+        private void btnRefresh_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            CreateCaptcha();
         }
     }
 }
